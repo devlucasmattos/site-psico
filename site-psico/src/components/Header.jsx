0626bd7c46627c4928dd9d3da0bd56fe797cf67a
devlucasmattos <lucasmattos312@gmail.com>
2024-10-30 23:02:40 -0300

    The CSS updates for the Header component ensure the paragraph text is centered and has a maximum width of 90%. A flexbox layout is used for better alignment, improving readability and aesthetics.
    The Header component's CSS now centers the paragraph text and limits its width to 90%, enhancing text alignment and maintaining a clean appearance.

--- site-psico/src/components/Header.jsx
+++ site-psico/src/components/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
             <div className="container">
                 <h1>Gabrielle Chemieski</h1>
                 <h2>CRP 07/39189</h2>
+                <div className="p">
                 <p>A transformação que você busca começa na sua mente. <br />Vamos caminhar juntos? 🧠⤵️</p>
+                </div>
                 <a href="https://api.whatsapp.com/message/3YQT5VMFXFKHI1?autoload=1&app_absent=0" className="btn-agendar">
                     <span>Agende sua consulta pelo WhatsApp</span>
                     
