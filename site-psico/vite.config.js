3c86ed427a43fd9d75ac749e36444ee676a6ac7f
devlucasmattos <lucasmattos312@gmail.com>
2024-10-31 00:03:44 -0300

    up to vercel

--- site-psico/vite.config.js
+++ site-psico/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/site-psico/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
